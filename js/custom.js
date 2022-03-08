aa21f8e2136ab2964a26d0b224e707780d49fe72
infifab <tmtg360@gmail.com>
2022-03-08 18:58:32 +0530

    navbar: dark mode button left;

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  element.classList.toggle("navbar-dark");
+}
+// Change the classList param so that the whole thing is not getting disrupted.
