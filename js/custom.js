554c25c01b7c2e0ce05c42b31b3c5068bcfc3666
infifab <tmtg360@gmail.com>
2022-03-08 20:28:54 +0530

    button state fixed; button color fixed; darkmode button animation to be done;

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,14 @@
 function myFunction() {
   var element = document.body;
+  var innerElement = document.getElementsByClassName("icon-bar");
+  var innerElement1 = document.getElementById("bttn1");
   element.classList.toggle("dark-mode");
+  
   element.classList.toggle("navbar-dark");
+  innerElement1.classList.toggle("navbar-toggler-dark");
+  for(i=0; i<innerElement.length; i++)
+  {
+    innerElement[i].classList.toggle('icon-bar-dark');
+  }
 }
 // Change the classList param so that the whole thing is not getting disrupted.
