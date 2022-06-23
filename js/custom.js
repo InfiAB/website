17e0af8a27ff9bba05ae3c84f511d130faa23408
Aditya Bansal <63427562+InfiAB@users.noreply.github.com>
2022-06-23 13:33:34 +0530

    Revert "."

--- js/custom.js
+++ js/custom.js
@@ -4,8 +4,6 @@ function myFunction() {
   var innerElement1 = document.getElementById("bttn1");
   var darkmodeButton = document.getElementsByClassName("dot");
   element.classList.toggle("dark-mode");
-
-  var loaders = document.getElementsByClassName("loader");
   
   element.classList.toggle("navbar-dark");
   innerElement1.classList.toggle("navbar-toggler-dark");
@@ -14,36 +12,18 @@ function myFunction() {
     innerElement[i].classList.toggle('icon-bar-dark');
   }
 
-  for(i=0; i<loaders.length; i++)
-  {
-    loaders[i].classList.toggle("loader-dark");
-  }
-
   for(i=0; i<darkmodeButton.length; i++)
   {
     darkmodeButton[i].classList.toggle("dot-dark");
   }
 }
 
-var loader="loader";
+
 const hours = new Date().getHours();
 if(hours > 20 && hours < 24) {
   myFunction();
-  loader = "loader-dark";
 }
 
 if(hours >= 0 && hours < 7) {
   myFunction();
-  loader = "loader-dark";
 }
-
-window.onload = function(){
-  setTimeout(function(){
-    // document.getElementById(loader).remove();
-    var loaders = document.getElementsByClassName(loader);
-    for(i=0; i<loaders.length; i++)
-    {
-      loaders[i].classList.remove();
-    }
-  },1000);
-};
